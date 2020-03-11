125cb194004827bdcfb7d406e75652ff81db7802
deama <deama@mail.com>
2020-03-11 16:50:46 +0000

    first commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,16 @@
+import java.util
+
 import javax.swing.text.StyledEditorKit.ForegroundAction
 
 import scala.swing.event.EditDone
 import swing._
 import Swing._
+import java.util.ArrayList
 
 object Main extends SimpleSwingApplication
 {
   var startMove = true
+  val arenaList :util.ArrayList[Button] = new util.ArrayList[Button]
 
   val arena:Array[Array[String]] = Array.ofDim[String](21,21)
 
@@ -18,19 +22,18 @@ object Main extends SimpleSwingApplication
     }
   }
 
-
   def top() = new MainFrame
   {
     title = "thing"
-    contents = new BoxPanel(Orientation.Vertical)
+    contents = new BoxPanel(Orientation.Horizontal)
     {
       for( i <- 0 to arena.length-1 by 1 )
       {
-        contents += new BoxPanel(Orientation.Horizontal)
+        contents += new BoxPanel(Orientation.Vertical)
         {
           for( j <- 0 to arena(0).length-1 by 1 )
           {
-            contents += new Button()
+            val button :Button = new Button()
             {
               font = new Font("Consolas", 0, 27)
               text = arena(i)(j)
@@ -42,6 +45,8 @@ object Main extends SimpleSwingApplication
               maximumSize = new Dimension(30,30)
               preferredSize = new Dimension(30,30)
             }
+            arenaList.add(button)
+            contents += button
           }
         }
       }
@@ -50,52 +55,73 @@ object Main extends SimpleSwingApplication
 
 
 
-  val snake:Snake = new Snake( 3, "O", "=", 10, 10, "red", "left" )
 
-  println( snake.getXPos() )
-  snake.getXPos() = 5
-  println( snake.getXPos() )
+  val snake:Snake = new Snake( 2, "O", "=", 10, 10, "red", "left" )
+
+
+  for( i <- 0 to snake.length by 1 )
+  {
+    if( i == 0 )
+    {
+      arena( snake.xPos )( snake.yPos ) = snake.head
+    }
+    else
+    {
+      arena( snake.xPos+(1*i) )( snake.yPos ) = snake.tail
+    }
+  }
 
   val snakeMove:Thread = new Thread
   {
     override def run(): Unit =
     {
-      Thread.sleep(1000)
+      Thread.sleep(2000)
+      //arenaList.get(0).text = "X"
+      point.text = "X"
+      //arena(5)(5) = "X"
+      println("THING")
+      println("THING")
+      println("THING")
+      println("THING")
+      println("THING")
+      /*
       while(startMove)
       {
-        snake.getDirection() match
+        snake.direction match
         {
           case "left" =>
           {
-            arena( snake.getXPos() )( snake.getYPos() )
+            for( i <- 0 to snake.length by 1 )
+            {
+              if( i == 0 )
+              {
+                snake.xPos = snake.xPos-1
+                arena( snake.xPos )( snake.yPos ) = snake.head
+              }
+              else
+              {
+                arena( snake.xPos+i )( snake.yPos ) = snake.tail
+              }
+            }
           }
         }
+        Thread.sleep(1000)
       }
+      */
     }
   }
   snakeMove.start()
 }
 
-class Snake(length:Int, head:String, tail:String, xPos:Int, yPos:Int, colour:String, direction:String )
+class Snake
+(
+  var length :Int,
+  val head :String,
+  val tail :String,
+  var xPos :Int,
+  var yPos :Int,
+  val colour :String,
+  var direction:String
+)
 {
-  val this.head = head
-  val this.tail = tail
-  val this.colour = colour
-
-  var this.xPos = xPos
-  var this.yPos = yPos
-  var this.length = length
-  var this.direction = direction
-
-  def getXPos() : Int = { return this.xPos }
-  def getYPos() : Int = { return this.yPos }
-  def getColur() : String = { return this.colour }
-  def getHead() : String = { return this.head }
-  def getTail() : String = { return this.tail }
-  def getLength() : Int = { return this.length }
-  def getDirection() : String = { return this.direction }
-
-  def setXPos() : Int = { return this.xPos }
-  def setYPos() : Int = { return this.yPos }
-  def setLength() : Int = { return this.length }
 }
